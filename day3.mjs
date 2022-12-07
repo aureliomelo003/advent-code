cdaad90dea67886c51c1bbb386853335dd534ddd
Aurelio Melo <aureliomelo003@gmail.com>
2022-12-06 21:28:04 -0500

    task almost fnished

--- day3.mjs
+++ day3.mjs
@@ -5,12 +5,26 @@ const lines = readFileSync("day3.txt", { encoding: "utf-8" }) // read day??.txt 
   .trim() // Remove starting/ending whitespace
   .split("\n") // Split on newline
 
+function letterToPriority(letter) {
+  if (/[a-z]/.test(letter)) {
+    letter.charCodeAt(0) - 96;
+  } else {
+    return letter.charCodeAt(0) - 65 + 27;
+  }
+}
+
 function part1() {
-  lines.map((line) => {
-    const halfOne = line.slice(0, line.length / 2);
-    const halfTwo = line.slice(line.length / 2);
-    console.log({halfOne, halfTwo});
-  })
+  const res = lines.map((line) => {
+    const halfOne = [...line.slice(0, line.length / 2)];
+    const halfTwo = [...line.slice(line.length / 2)];
+
+    let halfOneSet = new Set(halfOne);
+    const intersection = halfTwo.filter((x) => halfOneSet.has(x));
+    const dedub = [...new Set(intersection)];
+
+    return letterToPriority(dedub[0]);
+  });
+  console.log(res.reduce((a, b) => a + b, 0));
 }
 
 function part2() {
